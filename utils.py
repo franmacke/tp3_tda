9e90eb5a6adeccc1137fcf42bd39133f3ea94269
Franco <fmacke10@gmail.com>
2025-06-12 19:59:17 -0300

    Boilerplate

---
+++ utils.py
@@ -0,0 +1,13 @@
+
+
+def leer_archivo_conexiones(path):
+
+    with open(path, 'r') as f:
+        f.readline()
+
+        conexiones = []
+        for linea in f:
+            linea = linea.strip().split(',')
+            conexiones.append((linea[0], linea[1]))
+
+    return conexiones
