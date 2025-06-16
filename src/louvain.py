ab8add8e4fd51ddf551e47e98c4d237da0bbe4de
NSalvatierra <nsalvatierra@fi.uba.ar>
2025-06-16 12:06:50 -0300

    louvain

---
+++ src/louvain.py
@@ -0,0 +1,101 @@
+from collections import defaultdict
+import random
+from grafo import Grafo
+
+def calcular_modularidad(grafo, particion):
+    m = sum(len(grafo.vecinos(v)) for v in grafo.obtener_vertices()) / 2
+    grados = {v: len(grafo.vecinos(v)) for v in grafo.obtener_vertices()}
+    comunidades = defaultdict(set)
+    for nodo, comunidad in particion.items():
+        comunidades[comunidad].add(nodo)
+
+    modularidad = 0
+    for comunidad in comunidades.values():
+        for u in comunidad:
+            for v in comunidad:
+                A_uv = 1 if v in grafo.vecinos(u) else 0
+                modularidad += A_uv - (grados[u] * grados[v]) / (2 * m)
+    return modularidad / (2 * m)
+
+def una_iteracion_louvain(grafo, particion):
+    m = sum(len(grafo.vecinos(v)) for v in grafo.obtener_vertices()) / 2
+    grados = {v: len(grafo.vecinos(v)) for v in grafo.obtener_vertices()}
+    nodos = list(grafo.obtener_vertices())
+    mejora = True
+
+    while mejora:
+        mejora = False
+        random.shuffle(nodos)
+        for nodo in nodos:
+            comunidad_actual = particion[nodo]
+            vecinos = grafo.vecinos(nodo)
+
+            # Conteo de enlaces hacia cada comunidad vecina
+            enlaces_por_comunidad = defaultdict(int)
+            for vecino in vecinos:
+                comunidad_vecino = particion[vecino]
+                enlaces_por_comunidad[comunidad_vecino] += 1
+
+            mejor_delta = 0
+            mejor_comunidad = comunidad_actual
+
+            # Quito nodo de su comunidad temporalmente
+            particion[nodo] = -1
+            for comunidad, enlaces in enlaces_por_comunidad.items():
+                suma_grados = sum(grados[n] for n in particion if particion[n] == comunidad)
+                delta_q = enlaces - grados[nodo] * suma_grados / (2 * m)
+                if delta_q > mejor_delta:
+                    mejor_delta = delta_q
+                    mejor_comunidad = comunidad
+
+            if mejor_comunidad != comunidad_actual:
+                particion[nodo] = mejor_comunidad
+                mejora = True
+            else:
+                particion[nodo] = comunidad_actual
+
+    return particion
+
+def grafo_inducido(grafo, particion):
+    nuevo_grafo = Grafo()
+    comunidad_a_nodos = defaultdict(list)
+    for nodo, comunidad in particion.items():
+        comunidad_a_nodos[comunidad].append(nodo)
+
+    lista_comunidades = list(comunidad_a_nodos.values())
+    for i, comunidad_i in enumerate(lista_comunidades):
+        for j, comunidad_j in enumerate(lista_comunidades):
+            if i <= j:
+                # Cuento aristas entre comunidades
+                conexiones = 0
+                for u in comunidad_i:
+                    for v in comunidad_j:
+                        if u != v and v in grafo.vecinos(u):
+                            conexiones += 1
+                if conexiones > 0:
+                    nuevo_grafo.agregar_arista(i, j)
+    return nuevo_grafo, comunidad_a_nodos
+
+def algoritmo_louvain(grafo):
+    particion = {v: v for v in grafo.obtener_vertices()}
+    jerarquia = []
+
+    while True:
+        particion = una_iteracion_louvain(grafo, particion)
+        comunidades = defaultdict(list)
+        for nodo, comunidad in particion.items():
+            comunidades[comunidad].append(nodo)
+        jerarquia.append(dict(comunidades))
+
+        if len(set(particion.values())) == len(grafo.obtener_vertices()):
+            break
+
+        grafo, comunidad_a_nodos = grafo_inducido(grafo, particion)
+        # Reasigno nodos a nuevas comunidades
+        nueva_particion = {}
+        for nuevo_id, nodos in comunidad_a_nodos.items():
+            for nodo in nodos:
+                nueva_particion[nodo] = nuevo_id
+        particion = nueva_particion
+
+    return jerarquia
